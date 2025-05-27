853f538f69734b8addbb8d2c718ce8e2c619fb15
ViniPiai <vinicius-piai@hotmail.com>
2025-05-27 18:40:09 -0300

    Arrumado redirect

--- lib/main.router.dart
+++ lib/main.router.dart
@@ -10,14 +10,14 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:flutter/material.dart';
 import 'package:katana_router/katana_router.dart';
-import 'package:frontend/screens/agenda/agenda.dart' as _$2;
-import 'package:frontend/screens/doctors/doctors.dart' as _$4;
+import 'package:frontend/screens/agenda/agenda.dart' as _$1;
+import 'package:frontend/screens/doctors/doctors.dart' as _$2;
 import 'package:frontend/screens/home/home.dart' as _$3;
-import 'package:frontend/screens/new_appointment/new_appointment.dart' as _$6;
-import 'package:frontend/screens/new_patient/new_patient.dart' as _$7;
-import 'package:frontend/screens/patient_detail/patient_detail.dart' as _$5;
-import 'package:frontend/screens/patients/patients.dart' as _$8;
-import 'package:frontend/screens/sign_in/sign_in.dart' as _$1;
+import 'package:frontend/screens/new_appointment/new_appointment.dart' as _$4;
+import 'package:frontend/screens/new_patient/new_patient.dart' as _$5;
+import 'package:frontend/screens/patient_detail/patient_detail.dart' as _$7;
+import 'package:frontend/screens/patients/patients.dart' as _$6;
+import 'package:frontend/screens/sign_in/sign_in.dart' as _$8;
 export 'package:frontend/screens/agenda/agenda.dart' show AgendaPage;
 export 'package:frontend/screens/doctors/doctors.dart' show DoctorsPage;
 export 'package:frontend/screens/home/home.dart' show HomePage;
@@ -49,41 +49,41 @@ class AutoRouter extends AppRouter {
   }) : super(
           pages: pages ??
               [
-                _$1.SignInPage.query,
-                _$7.NewPatientPage.query,
-                _$5.PatientDetailPage.query,
-                _$8.PatientsPage.query,
-                _$6.NewAppointmentPage.query,
+                _$8.SignInPage.query,
+                _$5.NewPatientPage.query,
+                _$7.PatientDetailPage.query,
+                _$6.PatientsPage.query,
+                _$4.NewAppointmentPage.query,
                 _$3.HomePage.query,
-                _$4.DoctorsPage.query,
-                _$2.AgendaPage.query,
+                _$2.DoctorsPage.query,
+                _$1.AgendaPage.query,
               ],
         );
 
-  static const signInPage = _$1.SignInPage.query;
+  static const signInPage = _$8.SignInPage.query;
 
-  static const newPatientPage = _$7.NewPatientPage.query;
+  static const newPatientPage = _$5.NewPatientPage.query;
 
-  static const patientDetailPage = _$5.PatientDetailPage.query;
+  static const patientDetailPage = _$7.PatientDetailPage.query;
 
-  static const patientsPage = _$8.PatientsPage.query;
+  static const patientsPage = _$6.PatientsPage.query;
 
-  static const newAppointmentPage = _$6.NewAppointmentPage.query;
+  static const newAppointmentPage = _$4.NewAppointmentPage.query;
 
   static const homePage = _$3.HomePage.query;
 
-  static const doctorsPage = _$4.DoctorsPage.query;
+  static const doctorsPage = _$2.DoctorsPage.query;
 
-  static const agendaPage = _$2.AgendaPage.query;
+  static const agendaPage = _$1.AgendaPage.query;
 
   Map<RouteQueryBuilder, String> queryMap = {
-    _$1.SignInPage.query: "/sign_in",
-    _$7.NewPatientPage.query: "/patients/new",
-    _$5.PatientDetailPage.query: "/patients/detail/:uuid",
-    _$8.PatientsPage.query: "/patients",
-    _$6.NewAppointmentPage.query: "/new_appointment",
+    _$8.SignInPage.query: "/sign_in",
+    _$5.NewPatientPage.query: "/patients/new",
+    _$7.PatientDetailPage.query: "/patients/detail/:uuid",
+    _$6.PatientsPage.query: "/patients",
+    _$4.NewAppointmentPage.query: "/new_appointment",
     _$3.HomePage.query: "/home",
-    _$4.DoctorsPage.query: "/doctors",
-    _$2.AgendaPage.query: "/agenda",
+    _$2.DoctorsPage.query: "/doctors",
+    _$1.AgendaPage.query: "/agenda",
   };
 }
